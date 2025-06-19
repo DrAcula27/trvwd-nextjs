f5cf94f7ec3fcf8c592bff188122a9940e9840c8
Danielle <danielle@kendallspace.net>
2025-06-19 14:02:42 -0700

    add fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
   return (
-    // <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
     <footer className="row-start-3 flex flex-col gap-[24px] flex-wrap items-center justify-center">
       <div className="flex gap-[24px] flex-wrap">
         <a
@@ -8,17 +10,26 @@ export default function Footer() {
           href="https://www.facebook.com/trvcc/"
           target="_blank"
           rel="noopener noreferrer"
+          title="Tahuya River Valley Community Facebook Page"
         >
-          Tahuya River Valley Community Facebook Page
+          <FontAwesomeIcon
+            icon={faFacebook}
+            className="fa-2x"
+            color="#3a96ab"
+          />
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://www.facebook.com/groups/1638565539732130"
           target="_blank"
           rel="noopener noreferrer"
+          title="Crime Awareness In Tahuya River Valley & Surrounding Areas Facebook Group (Private)"
         >
-          Crime Awareness In Tahuya River Valley & Surrounding Areas
-          Facebook Group (Private)
+          <FontAwesomeIcon
+            icon={faFacebook}
+            className="fa-2x"
+            color="#3a96ab"
+          />
         </a>
       </div>
       <div className="flex gap-[24px] flex-wrap">
