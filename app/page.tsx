1d69c15f81f965fe3d6116f3782067bc38c08a85
Danielle <danielle@kendallspace.net>
2025-06-24 12:13:58 -0700

    style: update main layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Image
         src="/logo.png"
-        alt="Tahuya River Valley Water District logo"
+        alt="Tahuya River Valley Water District"
         width={256}
         height={128}
         priority
       />
     </main>
-    // </div>
   );
 }
