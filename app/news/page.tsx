46dae94e64fcbb8851deb4a399d403be9b809e67
Danielle <danielle@kendallspace.net>
2025-08-16 18:35:26 -0700

    style: remove grid row-start-2

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function News() {
   return (
-    <main className="row-start-2 flex flex-col items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20">
+    <main className="flex flex-col items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20">
       <h1>NEWS</h1>
       <Image
         src="/logo.png"
