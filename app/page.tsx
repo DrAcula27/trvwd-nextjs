ba1ad7c6697256c75ffbd12510f4397f057ac676
Danielle <danielle@kendallspace.net>
2025-08-17 22:22:53 -0700

    feat(issue-5): add resources section to home page with associated styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
-// import Image from 'next/image';
 import MeetingCard from '@/_components/MeetingCard';
+import MissionStatementCard from '@/_components/MissionStatementCard';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20">
-      <section className="">
-        {/* meeting card (takes up 1/3 of available width on screens >= 900px, then stacks vertically) with button that links to meetings page */}
-        <MeetingCard />
-        {/* mission statement card (takes up 2/3 of available width on screens >= 900px, then stacks vertically) with picture of service area and link to PDF of service map */}
+    <main className="page">
+      <section className="section-style-1">
+        <div className="home-hero">
+          <MeetingCard />
+
+          <MissionStatementCard />
+        </div>
       </section>
-      <section>
-        {/* mentions available resources to those who are residing within the water district with a button that links to the resources page */}
+      <section className="section-style-2">
+        <div className="home-section">
+          <h3 className="subtitle">Our Resources</h3>
+          <h2 className="title">
+            Tahuya River Valley Water District Resources
+          </h2>
+          <p>
+            Discover valuable information on water management,
+            conservation, billing, service requests, and more. Stay
+            informed, access forms, and find answers to common
+            questions. We are committed to providing you with the
+            resources you need for a sustainable water usage
+            experience.
+          </p>
+          <Link href="/resources" className="btn-primary btn-home">
+            View Resources
+          </Link>
+        </div>
       </section>
-      <section>
+      <section className="section">
         {/* mentions current news, showing a grid of the 4 most recent news stories, each with a short snippet and a read more link, with a button that links to the news page */}
       </section>
-      <section>
+      <section className="section">
         {/* mentions districts' testing and reporting policies with a button that links to the reports page */}
       </section>
     </main>
