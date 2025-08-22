3d15a4710a1b579973a3a874e8047a49ed82bc47
Danielle <danielle@kendallspace.net>
2025-08-22 15:33:30 -0700

    feat(issue-9): scaffold news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,15 +2,19 @@ import Image from 'next/image';
 
 export default function News() {
   return (
-    <main className="flex flex-col items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20">
-      <h1>NEWS</h1>
-      <Image
-        src="/images/logos/logo.png"
-        alt="Tahuya River Valley Water District"
-        width={256}
-        height={128}
-        priority
-      />
+    <main className="page">
+      <h1 className="title mt-6 p-4 text-center">News</h1>
+      <section className="bg-style-1">
+        <div className="page-content">
+          <Image
+            src="/images/logos/logo.png"
+            alt="Tahuya River Valley Water District"
+            width={256}
+            height={128}
+            priority
+          />
+        </div>
+      </section>
     </main>
   );
 }
