064771e9143a8b668cd564272b13a0c427be3e2d
Danielle <danielle@kendallspace.net>
2025-08-22 14:35:43 -0700

    feat(no-issue): add image, reorg page layout

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -6,31 +6,47 @@ export default function Staff() {
       <h1 className="title mt-6 p-4 text-center">
         Tahuya River Valley Water District Staff and Commission
       </h1>
-      <section className="bg-style-1 p-4 flex flex-col items-center justify-center sm:flex-row">
-        <div className="card max-w-fit mx-auto">
-          <h2 className="card-title">Staff</h2>
-          <h3 className="card-subtitle">
-            Drew Noble: System Manager
-          </h3>
+      <section className="bg-style-1 p-4">
+        <div className="max-w-6xl mx-auto flex flex-col gap-8 items-center justify-center sm:flex-row">
           <Image
-            src="/images/staff/Drew-Noble.png"
-            alt="Tahuya River Valley Water District"
-            width={512}
-            height={256}
+            src="/images/community/waterTower02.jpg"
+            alt="Water Tower - Tahuya River Valley Water District"
+            width={756}
+            height={1008}
             priority
-            className="rounded-lg"
+            className="rounded-lg shadow-lg mb-4 sm:mb-0 max-w-md h-auto"
           />
-        </div>
-        <div className="card max-w-fit mx-auto p-8">
-          <h2 className="card-title">Commissioners</h2>
-          <h3 className="card-subtitle">
-            Tahuya River Valley Water District
-          </h3>
-          <ul className="list-disc list-inside">
-            <li>President: Lisa Johnson</li>
-            <li>Treasurer: Pretoria Randles</li>
-            <li>Secretary: Danielle Andrews</li>
-          </ul>
+          <div className="card">
+            <h2 className="card-title">Staff</h2>
+            <h3 className="card-subtitle">
+              Drew Noble: System Manager
+            </h3>
+            <Image
+              src="/images/staff/Drew-Noble.png"
+              alt="Tahuya River Valley Water District"
+              width={512}
+              height={256}
+              priority
+              className="rounded-lg"
+            />
+            <div className="max-w-fit mt-8">
+              <h2 className="card-title">Commissioners</h2>
+              <ul className="list-disc list-inside">
+                <li>
+                  <span className="bold-text">President</span>: Lisa
+                  Johnson
+                </li>
+                <li>
+                  <span className="bold-text">Treasurer</span>:
+                  Pretoria Randles
+                </li>
+                <li>
+                  <span className="bold-text">Secretary</span>:
+                  Danielle Andrews
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     </main>
