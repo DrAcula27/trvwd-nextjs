a007738f7cf8fbe6270603c8c2c01346ce1be1d1
Danielle <danielle@kendallspace.net>
2025-11-22 13:42:34 -0800

    feat(issue-54): add google maps widget

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -1,7 +1,8 @@
-import MeetingCard from '@/_components/MeetingCard';
-import MeetingMinutesDropdown from '@/_components/MeetingMinutesDropdown';
-import PageTitle from '@/_components/PageTitle';
 import Link from 'next/link';
+import PageTitle from '@/_components/PageTitle';
+import MeetingCard from '@/_components/MeetingCard';
+import MeetingInfoCard from '@/_components/MeetingInfoCard';
+import MeetingMinutesDropdown from '@/_components/MeetingMinutesDropdown';
 
 export default function Meetings() {
   return (
@@ -83,46 +84,8 @@ export default function Meetings() {
               <MeetingCard />
             </div>
 
-            <div className="card max-w-2xl">
-              <h3 className="card-title">Meeting Information</h3>
-              <div className="space-y-4">
-                <p>
-                  TRVWD meetings are open to the public (except for
-                  rare executive sessions,{' '}
-                  <Link
-                    href={
-                      'http://apps.leg.wa.gov/RCW/default.aspx?cite=42.30.110'
-                    }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Executive sessions (RCW 42.30.110) - Washington State Legislature"
-                    aria-label="Executive sessions (RCW 42.30.110) - Washington State Legislature"
-                  >
-                    RCW 42.30.110
-                  </Link>
-                  ). We welcome everyone to attend our meetings.
-                  Public comments are welcome during regular business
-                  meeting, but may be limited at the discretion of the
-                  Commission.
-                </p>
-                <p>
-                  Regular meetings of the Water Commission are held on
-                  the fourth Tuesday of each month, beginning at 6pm,
-                  located in the{' '}
-                  <Link
-                    href={
-                      'https://www.google.com/maps/place/1665+NE+Tahuya+River+Dr,+Tahuya,+WA+98588/@47.4212847,-122.9742256,236m/data=!3m1!1e3!4m6!3m5!1s0x5491c831cf0cff11:0x11424294f5d50c8d!8m2!3d47.421165!4d-122.973268!16s%2Fg%2F11c27wg_sp?entry=ttu&g_ep=EgoyMDI0MDkxNi4wIKXMDSoASAFQAw%3D%3D'
-                    }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    title="Tahuya River Valley Community Club Building - Google Maps"
-                    aria-label="Tahuya River Valley Community Club Building - Google Maps"
-                  >
-                    Tahuya River Valley Community Club Building
-                  </Link>{' '}
-                  unless otherwise noted.
-                </p>
-              </div>
+            <div className="max-w-2xl">
+              <MeetingInfoCard />
             </div>
           </div>
         </div>
